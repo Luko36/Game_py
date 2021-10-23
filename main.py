13a4cf451f241559d053e404e905d0cc73edd801
Nikolas <michalickanikolas@gmail.com>
2021-10-23 23:30:45 +0200

    hah len tak

--- main.py
+++ main.py
@@ -1,7 +1,26 @@
+import time
 import pygame
+from pygame.event import event_name
 import random
 from config import *
 import sys
 
-print("ahoj svet")
- 
+print(screensize)
+pygame.init()
+
+screen = pygame.display.set_mode(screensize)
+pygame.display.set_caption("Pygame Test")
+
+
+time.sleep(1)
+running = True
+while running == True:
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    screen.fill(((32, 32, 32)))
+    pygame.display.update()
+pygame.quit()
+
