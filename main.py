21c604af4315010c8700739258edbe28a41ae395
Krypt0niT <82445388+Krypt0niT@users.noreply.github.com>
2021-10-23 23:24:57 +0200

    tvorba okna

--- main.py
+++ main.py
@@ -1,7 +1,23 @@
 import pygame
+from pygame.event import event_name
 import random
 from config import *
 import sys
 
-print("ahoj svet")
- 
+print(screensize)
+pygame.init()
+
+screen = pygame.display.set_mode(screensize)
+pygame.display.set_caption("Pygame Test")
+
+running = True
+while running == True:
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    screen.fill(((32, 32, 32)))
+    pygame.display.update()
+pygame.quit()
+
